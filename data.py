5e34c22f202a3dca84be25907f551ecf17479902
dlwlgus53 <dlwlgus4553@gmail.com>
2020-04-28 16:11:39 +0900

    0412 data load

--- data.py
+++ data.py
@@ -4,23 +4,23 @@ import torch
 class FSIterator:
     def __init__(self, filename, batch_size=32, just_epoch=False):
         self.batch_size = batch_size
-        self.just_epoch = just_epoch       
- 
-        self.end = open(filename + "/END.csv", 'r')
-        self.h_e = open(filename + "/H_E.csv", 'r')
-        self.high = open(filename + "/HIGH.csv", 'r')
-        self.l_e = open(filename + "/L_E.csv", 'r')
-        self.low = open(filename + "/LOW.csv", 'r')
-        self.ma5 = open(filename + "/MA5.csv", 'r')
-        self.ma10 = open(filename + "/MA10.csv", 'r')
-        self.s_e = open(filename + "/S_E.csv", 'r')
-        self.start = open(filename + "/START.csv", 'r')
-        self.trade = open(filename + "/TRADE.csv", 'r')
-        self.turnover = open(filename + "/TURNOVER.csv", 'r')
+        self.just_epoch = just_epoch        
+        self.fp_end = open(filename + "/END.csv", 'r')
+        self.fp_he = open(filename + "/H_E.csv", 'r')
+        self.fp_high = open(filename + "/HIGH.csv", 'r')
+        self.fp_le = open(filename + "/L_E.csv", 'r')
+        self.fp_low = open(filename + "/LOW.csv", 'r')
+        self.fp_ma5 = open(filename + "/MA5.csv", 'r')
+        self.fp_ma10 = open(filename + "/MA10.csv", 'r')
+        self.fp_se = open(filename + "/S_E.csv", 'r')
+        self.fp_start = open(filename + "/START.csv", 'r')
+        self.fp_trade = open(filename + "/TRADE.csv", 'r')
+        self.fp_turnover = open(filename + "/TURNOVER.csv", 'r')
 
 
-        self.fps = [self.end, self.h_e, self.high, self.l_e, self.low, self.ma5, self.ma10, self.s_e, self.start, self.trade, self.turnover]
-
+        self.fps = [self.fp_end, self.fp_he,self.fp_high, self.fp_le,
+        self.fp_low, self.fp_ma5, self.fp_ma10, self.fp_se, self.fp_start,
+        self.fp_trade, self.fp_turnover ]
 
     def __iter__(self):
         return self
@@ -30,23 +30,27 @@ class FSIterator:
             fp.seek(0)
 
     def __next__(self):
-        import pdb; pdb.set_trace()
+ 
         bat_seq = []
-
         touch_end = 0
 
         while(len(bat_seq)< self.batch_size):
-            split_seq = []
-            pre_seq = []
-        
-        
-            for i in range (0,len(self.fps)):
-                pre_seq.append([self.fps[i].readline()])
-
+            seq_end = self.fps[0].readline()
+            seq_he = self.fps[1].readline()
+            seq_high = self.fps[2].readline()
+            seq_le = self.fps[3].readline()
+            seq_low =self.fps[4].readline()
+            seq_ma5 = self.fps[5].readline()
+            seq_ma10 = self.fps[6].readline()
+            seq_se = self.fps[7].readline()
+            seq_start = self.fps[8].readline()
+            seq_trade = self.fps[9].readline()
+            seq_turnover = self.fps[10].readline()
+                
             if touch_end:
                 raise StopIteration
 
-            if len(pre_seq[0]) == 0:
+            if seq_end == "":
                 print("touch end")
                 touch_end = 1
 
@@ -60,26 +64,46 @@ class FSIterator:
                 '''                                                                                                                                                                                                                                                                                                                                              
                 self.reset()
                 # read the first line
-                for i in range (0,len(self.fps)):
-                    pre_seq.append([self.fps[i].readline()])
-            
 
-            for i in range (0,len(self.fps)):
-                split_seq.append([float(s) for s in pre_seq[i][0].split(',')])
+                seq_end = self.fps[0].readline()
+                seq_he = self.fps[1].readline()
+                seq_high = self.fps[2].readline()
+                seq_le = self.fps[3].readline()
+                seq_low =self.fps[4].readline()
+                seq_ma5 = self.fps[5].readline()
+                seq_ma10 = self.fps[6].readline()
+                seq_se = self.fps[7].readline()
+                seq_start = self.fps[8].readline()
+                seq_trade = self.fps[9].readline()
+                seq_turnover = self.fps[10].readline()
+
+
+
+            seq_end =  [float(s) for s in seq_end.split(',')]
+            seq_he =  [float(s) for s in seq_he.split(',')]
+            seq_high =  [float(s) for s in seq_high.split(',')]
+            seq_le =  [float(s) for s in seq_le.split(',')]
+            seq_low = [float(s) for s in seq_low.split(',')]
+            seq_ma5 =  [float(s) for s in seq_ma5.split(',')]
+            seq_ma10 =  [float(s) for s in seq_ma10.split(',')]
+            seq_se =  [float(s) for s in seq_se.split(',')]
+            seq_start =  [float(s) for s in seq_start.split(',')]
+            seq_trade =  [float(s) for s in seq_trade.split(',')]
+            seq_turnover =  [float(s) for s in seq_turnover.split(',')]                
 
             #if(np.count_nonzero(~np.isnan(seq_end))>7 and seq_end[-1] == 1):
             #if(np.count_nonzero(~np.isnan(seq_end)) >= 10 and np.count_nonzero(~np.isnan(seq_end)) < 21): # short data
             #if(np.count_nonzero(~np.isnan(seq_end)) >= 21): # long data
                 #if(np.count_nonzero(~np.isnan(seq_f))>4):
-            if(~(np.isnan(split_seq).any())):
-                import pdb; pdb.set_trace()
 
-            seqs = split_seq
-            bat_seq.append(seqs)
-        
-        
+            if(sum(~np.isnan(seq_end)) == sum(~np.isnan(seq_start)) == sum(~np.isnan(seq_low))== sum(~np.isnan(seq_high))):
+                if(max(seq_end)<=2):
+                    seqs = [seq_end, seq_he, seq_high, seq_high, seq_le, seq_low, seq_ma5,
+                    seq_ma10, seq_se, seq_start, seq_start, seq_trade, seq_turnover]
+                    bat_seq.append(seqs)
+                
         x_data, y_data, mask_data = self.prepare_data(np.array(bat_seq))#B x [[E*daylen],[S*daylen],[L*daylen],[H*daylen]]
-        import pdb; pdb.set_trace()
+        
         device = torch.device("cuda")
         x_data = torch.tensor(x_data).type(torch.float32).to(device)
         y_data = torch.tensor(y_data).type(torch.LongTensor).to(device)
@@ -121,7 +145,7 @@ class FSIterator:
         target = []
         temp = []
         seq_end_x = seq[:,0,:-1]
-        import pdb; pdb.set_trace()
+        #import pdb; pdb.set_trace()
         for l in seq_end_x:
             temp = [(np.argmax(l) - i) for i in range(0, len(l))]
             temp = [i if i >= 0 else 0 for i in temp]
@@ -133,55 +157,98 @@ class FSIterator:
     
     def prepare_data(self, seq):
         PRE_STEP = 1 # this is for delta
-        import pdb; pdb.set_trace()
-        seq_x = []
-        seq_delta = []
-        x_data = []
+        #import pdb; pdb.set_trace()
+        seq_end_x = self.trimBatch(seq[:,0,:-1])
+        seq_he_x = self.trimBatch(seq[:,1,:-1])
+        seq_high_x = self.trimBatch(seq[:,2,:-1])
+        seq_le_x = self.trimBatch(seq[:,3,:-1])
+        seq_low_x = self.trimBatch(seq[:,4,:-1])
+        seq_ma5_x = self.trimBatch(seq[:,5,:-1])
+        seq_ma10_x = self.trimBatch(seq[:,6,:-1])
+        seq_se_x = self.trimBatch(seq[:,7,:-1])
+        seq_start_x = self.trimBatch(seq[:,8,:-1])
+        seq_trade_x = self.trimBatch(seq[:,9,:-1])
+        seq_turnover_x = self.trimBatch(seq[:,10,:-1])
 
-        for i in range(0,len(self.fps)):
-            seq_x.append(seq[:,i,:-1]) #append each factors of all batches # B, Factors, -1
-
-
+        
+       
         seq_y =  self.make_target(seq)
         
 
         # resize into the longest day length
-        for i in range(0,len(self.fps)):
-            seq_x[i] = self.trimBatch(seq_x[i])
 
-        seq_mask = self.getMask(seq_x[0][:,1:-PRE_STEP])
-
-
-        for i in range(0,len(self.fps)):
-            seq_x[i] = np.nan_to_num(seq_x[i])
-
-
-        for i in range(0,len(self.fps)):
-
-            seq_delta.append(seq_x[i][:,1:] - seq_x[i][:,:-1])
-
+        '''
+        seq_end_x = self.trimBatch(seq_end_x)
+        seq_start_x = self.trimBatch(seq_start_x)
+        seq_low_x = self.trimBatch(seq_low_x)
+        seq_high_x = self.trimBatch(seq_high_x)
+        '''
         
+        seq_mask = self.getMask(seq_end_x[:,1:-PRE_STEP])
+        
+        seq_end_x = np.nan_to_num(seq_end_x)
+        seq_start_x = np.nan_to_num(seq_start_x)
+        seq_low_x = np.nan_to_num(seq_low_x)
+        seq_high_x = np.nan_to_num(seq_high_x)
+
+        seq_end_x = np.nan_to_num(seq_end_x)
+        seq_he_x = np.nan_to_num(seq_he_x)
+        seq_high_x = np.nan_to_num(seq_high_x)
+        seq_le_x = np.nan_to_num(seq_le_x)
+        seq_low_x = np.nan_to_num(seq_low_x)
+        seq_ma5_x = np.nan_to_num(seq_ma5_x)
+        seq_ma10_x = np.nan_to_num(seq_ma10_x)
+        seq_se_x = np.nan_to_num(seq_se_x)
+        seq_start_x = np.nan_to_num(seq_start_x)
+        seq_trade_x = np.nan_to_num(seq_trade_x)
+        seq_turnover_x = np.nan_to_num(seq_turnover_x)
+
+
+
+        seq_end_x_delta = seq_end_x[:,1:] - seq_end_x[:,:-1]
+        seq_start_x_delta = seq_start_x[:,1:] - seq_start_x[:,:-1]
+        seq_low_x_delta = seq_low_x[:,1:] - seq_low_x[:,:-1]
+        seq_high_x_delta = seq_high_x[:,1:] - seq_high_x[:,:-1]
+
+
+        seq_end_xd =  seq_end_x[:,1:] - seq_end_x[:,:-1]
+        seq_he_xd =  seq_he_x[:,1:] - seq_he_x[:,:-1]
+        seq_high_xd =  seq_high_x[:,1:] - seq_high_x[:,:-1]
+        seq_le_xd =  seq_le_x[:,1:] - seq_le_x[:,:-1]
+        seq_low_xd =  seq_low_x[:,1:] - seq_low_x[:,:-1]
+        seq_ma5_xd =  seq_ma5_x[:,1:] - seq_ma5_x[:,:-1]
+        seq_ma10_xd =  seq_ma10_x[:,1:] - seq_ma10_x[:,:-1]
+        seq_se_xd =  seq_se_x[:,1:] - seq_se_x[:,:-1]
+        seq_start_xd =  seq_start_x[:,1:] - seq_start_x[:,:-1]
+        seq_trade_xd =  seq_trade_x[:,1:] - seq_trade_x[:,:-1]
+        seq_turnover_xd = seq_turnover_x[:,1:] - seq_turnover_x[:,:-1]
+
+
+
+
+
+
+
+
         try : 
-            for i in range(0,len(self.fps)):
+            x_data = np.stack([seq_end_x[:,1:-PRE_STEP], seq_end_xd[:,:-PRE_STEP],
+                           seq_he_x[:,1:-PRE_STEP], seq_he_xd[:,:-PRE_STEP],
+                           seq_high_x[:,1:-PRE_STEP], seq_high_xd[:,:-PRE_STEP],
+                           seq_le_x[:,1:-PRE_STEP], seq_le_xd[:,:-PRE_STEP],
+                           seq_low_x[:,1:-PRE_STEP], seq_low_xd[:,:-PRE_STEP],
+                           seq_ma5_x[:,1:-PRE_STEP], seq_ma5_xd[:,:-PRE_STEP],
+                           seq_ma10_x[:,1:-PRE_STEP], seq_ma10_xd[:,:-PRE_STEP],
+                           seq_se_x[:,1:-PRE_STEP], seq_se_xd[:,:-PRE_STEP],
+                           seq_start_x[:,1:-PRE_STEP], seq_start_xd[:,:-PRE_STEP],
+                           seq_trade_x[:,1:-PRE_STEP], seq_trade_xd[:,:-PRE_STEP],
+                           seq_turnover_x[:,1:-PRE_STEP], seq_turnover_xd[:,:-PRE_STEP]], axis=2) #batch * daylen * inputdim(2)
 
-                x_data.append(seq_x[i][:,1:-PRE_STEP])
-                x_data.append(seq_delta[i][:,:-PRE_STEP]) # factors, batch_Size, daylen
- 
-            '''
-            x_data = np.stack([seq_end_x[:,1:-PRE_STEP], seq_end_x_delta[:,:-PRE_STEP],
-                           seq_start_x[:,1:-PRE_STEP], seq_start_x_delta[:,:-PRE_STEP],
-                           seq_low_x[:,1:-PRE_STEP], seq_low_x_delta[:,:-PRE_STEP],
-                           seq_high_x[:,1:-PRE_STEP], seq_high_x_delta[:,:-PRE_STEP]], axis=2) #batch * daylen * inputdim(2)
-            '''
         except:
-            print("error")
             import pdb; pdb.set_trace()
-
-    # factors, batch, daylen
-        #x_data = np.array(x_data).transpose(1,0,2) # daylen * batch * inputdim
-        x_data = np.array(x_data).transpose(2,1,0)
+        x_data = x_data.transpose(1,0,2) # daylen * batch * inputdim
+        
         #y_data = seq_y.reshape(1,-1) # batch * 1
-        y_data = np.stack([seq_y.transpose(1,0)])# 1*batch*datlen...?????!
+        y_data = np.stack([seq_y.transpose(1,0)])# 1*batch*1
 
         #y_data = (seq_delta[:,1:] > 0)*1.0 # the diff
         
@@ -198,19 +265,14 @@ if __name__ == "__main__":
     import numpy as np
 
     #filename = os.environ['HOME']+'/FinSet/data/GM.csv.seq.shuf'
-    filename = "../data/0412/regression/train"
+    filename = "../data/dummy/classification_train.csv"
     #df_train = pd.read_csv("../data/dummy/classification_train.csv")
     bs = 4
     train_iter = FSIterator(filename, batch_size=bs, just_epoch=True)
 
-    for input, target, mask in train_iter: 
-        print(input)
-        print(input.shape)
-        print(target.shape)
-        print(mask.shape)
-
-
-
-        break
-
-                    
+    i = 0
+    for tr_x, tr_y, tr_m, end_of_data in train_iter:
+        print(i, tr_x, tr_y, tr_m)
+        i = i + 1
+        if i > 2:
+            break
--- train.py
+++ train.py
@@ -8,26 +8,27 @@ from data import FSIterator
 
 import numpy as np
 import pandas as pd
-import sys
+
+from sklearn.metrics import recall_score, f1_score, precision_score
 
 
 def train_main(args, model, train_path, criterion, optimizer):
-    iloop = 0
-    current_loss = 0
-    all_losses = []
+    iloop=0
+    current_loss =0
+    all_losses = [] 
     batch_size = args.batch_size
-    train_iter = FSIterator(train_path, batch_size)
-
-    for input, target, mask in train_iter:  # TODO for debugging
+    train_iter = FSIterator(train_path, batch_size) 
+    for input,target, mask in train_iter: #TODO for debugging
         loss = train(args, model, input, mask, target, optimizer, criterion)
         current_loss += loss
-
+        
         if (iloop+1) % args.logInterval == 0:
             print("%d %.4f" % (iloop+1, current_loss/args.logInterval))
-            all_losses.append(current_loss / args.logInterval)
-            current_loss = 0
+            all_losses.append(current_loss /args.logInterval)
+            current_loss=0
+            
 
-        iloop += 1
+        iloop+=1
 
 
 def train(args, model, input, mask, target, optimizer, criterion):
@@ -35,20 +36,19 @@ def train(args, model, input, mask, target, optimizer, criterion):
     loss_matrix = []
     optimizer.zero_grad()
 
-    output = model(input)
-
-    for t in range(input.size(0)-1):
+    output, hidden = model(input)
+    
+    for t in range(input.size(0)):
         #import pdb; pdb.set_trace()
         loss = criterion(output[t].view(args.batch_size, -1),
-                        target[0][t].view(-1,1))
-                           
-        loss_matrix.append(loss.view(1, -1))
-
+                        target[0][t])
+        loss_matrix.append(loss.view(1,-1))
+    #import pdb; pdb.set_trace()
     loss_matrix = torch.cat(loss_matrix, dim=0)
 
-    masked = loss_matrix * mask[1:]
+    masked = loss_matrix * mask
 
-    loss = torch.sum(masked) / torch.sum(mask[1:])
+    loss = torch.sum(masked) / torch.sum(mask)
 
     loss.backward()
 
@@ -57,79 +57,132 @@ def train(args, model, input, mask, target, optimizer, criterion):
     return loss.item()
 
 
+
 def evaluate(args, model, input, mask, target, criterion):
-    
     loss_matrix = []
-    err_matrix = []
+    acc_matrix = []
+    recall_matrix = []
+    f1_matrix = []
+    precision_matrix = []
+
     daylen = args.daytolook
-    output = model(input)
+    output, hidden = model(input)
+    
+    #input : daylen * batchsize
 
     '''Part of loss'''
-    #for t in range(input.size(0)-1):
-    #print(input.size(0)-1)
-    for t in range(input.size(0)-1):
-        loss = criterion(output[t].view(args.batch_size, -1),
-                        target[0][t].view(-1,1))
-                         #input[t+1][:, 0].view(args.batch_size, -1)) 
-        err = abs(((target[0][t].view(-1,1)) - output[t].view(args.batch_size, -1))/(target[0][t].view(-1,1)+sys.float_info.epsilon))
+    for t in range(input.size(0)):
+        loss = criterion(output[t].view(args.batch_size,-1),
+                    target[0][t])
 
-        loss_matrix.append(loss.view(1, -1))
-        err_matrix.append(err.view(1,-1))
+        loss_matrix.append(loss.view(1,-1))
 
     loss_matrix = torch.cat(loss_matrix, dim=0)
-    err_matrix = torch.cat(err_matrix, dim=0)
-    
-    masked_loss = loss_matrix * mask[1:]  # TODO
-    masked_err = err_matrix * mask[1:]
 
-    '''
-    masked_err[masked_err == float('inf')] = np.nan
-    masked_err[np.isnan(masked_err.cpu())] =np.nanmax(masked_err.cpu())
-    '''
+    masked = loss_matrix * mask
+    lossPerDay = torch.sum(masked, dim = 1)/torch.sum(mask, dim=1 ) #1*daylen
+    loss = torch.sum(masked[:daylen]) / torch.sum(mask[:daylen])
 
-    lossPerDay = torch.sum(masked_loss, dim=1) / \
-        torch.sum(mask[1:], dim=1)  # 1*daylen
-    errPerDay = torch.sum(masked_err, dim=1) / \
-        torch.sum(mask[1:], dim=1)  # 1*daylen
-    
-    loss = torch.sum(masked_loss[:daylen]) / torch.sum(mask[1:][:daylen])
-    err = torch.sum(masked_err[:daylen]) / torch.sum(mask[1:][:daylen])
+    '''Part of accuracy'''
+    #import pdb; pdb.set_trace()
+    for t in range(input.size(0)):
+        #import pdb; pdb.set_trace()
+        result = torch.max(output[t].data, 1)[1]
+        accuracy = (target.squeeze()[t] == result)
+        acc_matrix.append((accuracy).view(1,-1))
+        # if(t==0):
+        #     print(torch.max(output[t].data, 1)[1])
+        #     print((target.squeeze()[t]))
 
     
-    return lossPerDay, loss.item(), errPerDay, err.item()
+
+    acc_matrix = torch.cat(acc_matrix, dim=0)
+    #import pdb; pdb.set_trace()
+    
+    masked_acc = acc_matrix * mask
+    accPerDay = torch.sum(masked_acc, dim =1)/torch.sum(mask, dim=1) #TODO actually, don't need mask
+    accuracy = torch.sum(masked_acc[:daylen])/torch.sum(mask[:daylen])
+    
+    '''Part of recall'''
+    for t in range(daylen):
+        try:
+            #import pdb; pdb.set_trace()
+            result = torch.max(output[t].data,1)[1]
+            recall = recall_score(target.squeeze()[t].cpu(), result.cpu(), average='macro')    
+            precision = precision_score(target.squeeze()[t].cpu(), result.cpu(), average='macro')
+            f1 = f1_score(target.squeeze()[t].cpu(), result.cpu(), average='macro')       
+
+            recall_matrix.append(recall)    
+            precision_matrix.append(precision)    
+            f1_matrix.append(f1)         
+        except:
+            import pdb; pdb.set_trace()
+    #recall_matirx = torch.cat(recall_matrix, dim=0)
+
+        
+        
+    
+
+    return  f1_matrix, precision_matrix, recall_matrix, accPerDay, accuracy.item(), lossPerDay, loss.item()
+
+
+
+
 
 
 def test(args, model, test_path, criterion):
     current_loss = 0
-    current_err = 0
+    current_acc =0
+    current_recall =0 
+    
     lossPerDays = []
-    errPerDays = []
+    accPerDays = []
+    recallPerDays = []
+    precisionPerDays = []
+    f1PerDays = []
+
     lossPerDays_avg = []
-    errPerDays_avg = []
-    model.eval()
+    accPerDays_avg = []
+    recallPerDays_avg = []
+    precisionPerDays_avg = []
+    f1PerDays_avg = []
+
+    model = model.eval()
 
     daylen = args.daytolook
     with torch.no_grad():
-        iloop = 0
+        iloop =0
         test_iter = FSIterator(test_path, args.batch_size, 1)
         for input, target, mask in test_iter:
-            #TODO errorPerDay, error
-            lossPerDay, loss, errPerDay, err = evaluate(
-                args, model, input, mask, target, criterion)
-            lossPerDays.append(lossPerDay[:daylen])  # n_batches * 10
-            errPerDays.append(errPerDay[:daylen])
+
+            f1PerDay, precisionPerDay, recallPerDay, accPerDay, acc, lossPerDay, loss = evaluate(args, model, input, mask, target, criterion)
+            lossPerDays.append(lossPerDay[:daylen]) #n_batches * 10
+            accPerDays.append(accPerDay[:daylen])
+            recallPerDays.append(recallPerDay)
+            precisionPerDays.append(precisionPerDay)
+            f1PerDays.append(f1PerDay)
+
+            current_acc += acc
             current_loss += loss
-            current_err += err
-            iloop += 1
-
+            iloop+=1
+        
+        
         lossPerDays = torch.stack(lossPerDays)
-        errPerDays = torch.stack(errPerDays)
-        lossPerDays_avg = lossPerDays.sum(dim=0)
-        errPerDays_avg = errPerDays.sum(dim=0)
-        lossPerDays_avg = lossPerDays_avg/iloop
-        errPerDays_avg = errPerDays_avg/iloop
+        lossPerDays_avg = (lossPerDays.sum(dim =0))/iloop
 
+        accPerDays = torch.stack(accPerDays)
+        accPerDays_avg = (accPerDays.sum(dim = 0))/iloop
+
+        #recallPerDays = torch.stack(recallPerDays)
+        recallPerDays_avg = torch.FloatTensor(recallPerDays).sum(dim=0)/iloop
+        precisionPerDays_avg = torch.FloatTensor(precisionPerDays).sum(dim=0)/iloop
+        f1PerDays_avg = torch.FloatTensor(f1PerDays).sum(dim=0)/iloop
+        
+        
+        #lossPerDays_avg = lossPerDays_avg/iloop
+        #accPerDays_avg = accPerDays_avg/iloop
+
+        current_acc = current_acc/iloop
         current_loss = current_loss/iloop
-        current_err = current_err/iloop
-            #TODO errPerDays_avg, current_loss
-    return lossPerDays_avg, current_loss, errPerDays_avg, current_err
+
+    return  precisionPerDays_avg, f1PerDays_avg, recallPerDays_avg, accPerDays_avg, current_acc, lossPerDays_avg, current_loss
